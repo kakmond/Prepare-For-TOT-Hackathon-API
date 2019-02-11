7ad8db7cdb9b8b41d250b2e547f0551b08fae356
wasuthun wanaphongthipakorn <pond_Zen@hotmail.com>
2019-02-11 16:16:50 +0700

    fix error

--- functions/index.js
+++ functions/index.js
@@ -18,9 +18,6 @@ main.use('/api/', app)
 // Expose Express API as a single Cloud Function:
 exports.api = functions.https.onRequest(main)
 
-<<<<<<< HEAD
-app.use('/aqi', indexRouter)
-=======
 app.use('/news', indexRouter)
 app.use('/user', userRouter)
->>>>>>> fe31688f4a25060f024ee6c3fb0352c6f93b8665
+
