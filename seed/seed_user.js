f86011ed3381dd58e3c7f41c871356492512b0ae
wasuthun wanaphongthipakorn <pond_Zen@hotmail.com>
2019-02-11 14:04:19 +0700

    finish seed data

---
+++ seed/seed_user.js
@@ -0,0 +1,27 @@
+const { admin } = require('./config/firebaseConfig')
+
+const newsCollection = admin.firestore().collection('User')
+
+const news = [
+  {
+    user:{
+        name: "kong",
+        lastname: "assavavisidchai",
+        email:"kongzahahaplus@gmail.com",
+        id:"5910545833",
+        workplace:"Garena",
+        telephone:"1150"
+    }
+  }
+]
+
+for (var i = 0; i < news.length; i++) {
+  newsCollection
+    .add(news[i])
+    .then(function (docRef) {
+      console.log('Document written with ID: ', docRef.id)
+    })
+    .catch(function (error) {
+      console.error('Error adding document: ', error)
+    })
+}
